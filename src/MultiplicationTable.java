917e5101bc6aec096de63a4f6c2c84544374fd68
p-suryavanshi <pavans6350@gmail.com>
2026-03-03 11:37:35 +0530

    Multiplication table

---
+++ src/MultiplicationTable.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class MultiplicationTable {
+
+    public static void printMultiplicationTable(int number) {
+
+        int i = 1;
+
+        while (i <= 10) {
+            System.out.println(number + " X " + i + " = " + (number * i) );
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\n--- Welcome to the Multiplication table Calculator ---\n");
+
+        System.out.print("Please enter a number whose Multiplication table you want to print: ");
+        int number = input.nextInt();
+
+        printMultiplicationTable(number);
+
+    }
+
+}
