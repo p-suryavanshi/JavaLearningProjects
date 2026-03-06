f6756455fe7923ec22868c93e088ff934bf38508
p-suryavanshi <pavans6350@gmail.com>
2026-03-06 21:45:25 +0530

    Factorial Calculator

---
+++ src/FactorialCalculator.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class FactorialCalculator {
+
+    static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\n*** Welcome to Factorial Calculator ***\n");
+
+        System.out.print("Please enter a factorial number that you want to calculate: ");
+        int num = input.nextInt();
+
+        long fact = factorial(num);
+        System.out.println("The Factorial of " + num + " is: " + fact);
+
+    }
+
+    public static long factorial(int num){
+
+        int n = 1;
+        long fact = 1;
+        while ( n <= num ){
+            fact *= n;
+            n++;
+        }
+
+        return fact;
+    }
+
+}
