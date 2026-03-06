3ffcdedf38d7d87bf8883c648e581c0fbcc2dd48
p-suryavanshi <pavans6350@gmail.com>
2026-03-06 22:59:48 +0530

    Created program of multiplying two floating point numbers

---
+++ src/MultiplyTwoFloatingNumbers.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class MultiplyTwoFloatingNumbers {
+
+    static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\n--- Welcome to the Multiplying two floating point numbers ---\n");
+
+        System.out.print("Please enter a floating point number to multiply: ");
+        float num1 = input.nextFloat();
+
+        System.out.print("Now, enter the second number to multiply: ");
+        float num2 = input.nextFloat();
+
+        float mult = num1 * num2;
+
+        System.out.println("\nThe result of your given number is: " + mult);
+
+    }
+
+}
