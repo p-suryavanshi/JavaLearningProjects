c98aed85a6c49a841c1f935155edc5491d79238e
p-suryavanshi <pavans6350@gmail.com>
2026-03-06 23:20:44 +0530

    Fahrenheit to celsius converter

---
+++ src/FahrenheitToCelsius.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class FahrenheitToCelsius {
+
+    static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\n--- Welcome to the Fahrenheit to Celsius Converter ---\n");
+
+        System.out.println("Please enter the Temperature in Fahrenheit: ");
+        float fahrenheit = input.nextFloat();
+
+        double celsius = (fahrenheit - 32) / 1.8;
+
+        System.out.println("The temperature after converting Fahrenheit to  Celsius: " + celsius );
+
+    }
+
+}
