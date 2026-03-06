bd38b9a91c9501074ec4d5911c183d5e9252638c
p-suryavanshi <pavans6350@gmail.com>
2026-03-06 22:45:55 +0530

    Program of additing two interger numbers

---
+++ src/AddTwoIntergers.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class AddTwoIntergers {
+
+    static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\n--- Welcome to the Additing two intergers and printing the result program ---\n");
+
+        System.out.println("Please enter first interger number to add: ");
+        int num1 = input.nextInt();
+
+        System.out.println("Now, enter the second interger number to add: ");
+        int num2 = input.nextInt();
+
+        int add = num1 + num2;
+
+        System.out.println("\nThe result of additing two intergers is: " + add);
+
+    }
+
+}
