f1a90d44611abcc818d0a05dca3fdf2535f5f959
p-suryavanshi <pavans6350@gmail.com>
2026-03-07 16:58:44 +0530

    Minutes to Hours & Minutes Converter

--- src/Basic/MinutesIntoHoursAndMinutesConverter.java
+++ src/Basic/MinutesIntoHoursAndMinutesConverter.java
@@ -1,4 +1,21 @@
 package Basic;
+import java.util.*;
 
 public class MinutesIntoHoursAndMinutesConverter {
+
+    static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\n--- Welcome to the Minutes into hourse and Minutes Converter ---\n");
+
+        System.out.print("Please enter the Minutes: ");
+        int minut = input.nextInt();
+
+        float hoursAndMinutes = minut / 60F;
+
+        System.out.println("Minutes is converted into Hours & Minutes: " + hoursAndMinutes + "Hours");
+
+    }
+
 }
