1de4b2731703996eddfa3d1483fb8afe8c48ea6e
p-suryavanshi <pavans6350@gmail.com>
2026-03-07 17:02:19 +0530

    Perimeter of a Rectangle Calculator

--- src/Basic/PerimeterOfRectangle.java
+++ src/Basic/PerimeterOfRectangle.java
@@ -1,4 +1,24 @@
 package Basic;
+import java.util.*;
 
 public class PerimeterOfRectangle {
+
+    static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\n--- Welcome to the Perimeter of Rectangle Calculator ---\n");
+
+        System.out.print("Please enter the length of the rectangle: ");
+        float length = input.nextFloat();
+
+        System.out.print("Now, enter the width of the rectangle: ");
+        float width = input.nextFloat();
+
+        double perimeter  = 2 * ( length + width ) ;
+
+        System.out.println("The Perimeter of Rectangle is: " + perimeter);
+
+    }
+
 }
