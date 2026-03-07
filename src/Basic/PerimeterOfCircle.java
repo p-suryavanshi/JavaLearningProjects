27ea4cae472e63dcc7c4b7ad13f00a389d28c99a
p-suryavanshi <pavans6350@gmail.com>
2026-03-07 17:01:33 +0530

    Perimeter of a Circle Calculator

--- src/Basic/PerimeterOfCircle.java
+++ src/Basic/PerimeterOfCircle.java
@@ -1,4 +1,21 @@
 package Basic;
+import java.util.*;
 
 public class PerimeterOfCircle {
+
+    static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\n=== Welcome to the Perimeter of Circle Calculator ===\n");
+
+        System.out.print("Please enter the radius (length of circle to center) of circle: ");
+        float radius  = input.nextFloat();
+
+        double  perimeter = 2 * 3.142857142857  * radius;
+
+        System.out.println("\nThe Perimeter of the Circle is: " + perimeter);
+
+    }
+
 }
