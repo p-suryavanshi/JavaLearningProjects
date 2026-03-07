5b8f73a093d2dbc3db9cd770e1f54354812619a9
p-suryavanshi <pavans6350@gmail.com>
2026-03-07 16:52:23 +0530

    Area of a Circle Calculator

--- src/Basic/AreaOfCircle.java
+++ src/Basic/AreaOfCircle.java
@@ -1,4 +1,21 @@
 package Basic;
+import java.util.*;
 
 public class AreaOfCircle {
+
+    static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\n--- Welcome to the Area of Circle Calculator ---\n");
+
+        System.out.print("Please enter the radius (length of circle to center) of your circle: ");
+        float radius = input.nextFloat();
+
+        double circle  = 3.14159265359 * radius * radius;
+
+        System.out.println("The area of circle is: "  + circle);
+
+    }
+
 }
