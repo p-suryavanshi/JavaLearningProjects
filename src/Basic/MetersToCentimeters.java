c5e01bcbba95b43581af511fee7b34f171b1047c
p-suryavanshi <pavans6350@gmail.com>
2026-03-07 16:57:50 +0530

    Meter to Centimeter Converter

--- src/Basic/MetersToCentimeters.java
+++ src/Basic/MetersToCentimeters.java
@@ -1,4 +1,21 @@
 package Basic;
+import java.util.*;
 
 public class MetersToCentimeters {
+
+    static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\n--- Welcome to the Meter to Centimeter Converter ---\n");
+
+        System.out.print("Please enter the meter: ");
+        double meter = input.nextDouble();
+
+        double centimeter = meter * 100;
+
+        System.out.println("Meter: " + meter + "m" + "  is converted into centimeter: " + centimeter + "cm");
+
+    }
+
 }
