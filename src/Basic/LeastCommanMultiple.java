c8b247813e1c24f463926506d0b9df1f63a38a57
p-suryavanshi <pavans6350@gmail.com>
2026-03-07 17:13:17 +0530

    Least Comman Multiplyer

---
+++ src/Basic/LeastCommanMultiple.java
@@ -0,0 +1,28 @@
+package Basic;
+
+import java.util.*;
+
+public class LeastCommanMultiple {
+
+    static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\n*** Welcome to the Lowest Comman Multiple Number Calculator ***\n");
+
+        System.out.print("Please enter a number: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Now, enter the second number: ");
+        int num2 = input.nextInt();
+
+        int result = leastCommanMultiple();
+
+    }
+
+    public static int leastCommanMultiple(){
+
+        return 0;
+    }
+
+}
