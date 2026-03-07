ae2b97ae09876555165c6583c7b270997b7a9c34
p-suryavanshi <pavans6350@gmail.com>
2026-03-07 18:20:16 +0530

    Square of a Number Finder

---
+++ src/Basic/SquareOfNumber.java
@@ -0,0 +1,21 @@
+package Basic;
+import java.util.*;
+
+public class SquareOfNumber {
+
+    static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\n--- Welcome To the Square of a Number Finder ---\n");
+
+        System.out.print("Please enter a number: ");
+        int num = input.nextInt();
+
+        int square = num * num;
+
+        System.out.println("The Square of your given number is: " + square );
+
+    }
+
+}
