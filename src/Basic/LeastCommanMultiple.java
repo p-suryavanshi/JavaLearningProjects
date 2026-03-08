9e0d92a860331d5bbdf3d43f0164304c91c7fa7f
p-suryavanshi <pavans6350@gmail.com>
2026-03-08 07:31:01 +0530

    Lcm Finder

--- src/Basic/LeastCommanMultiple.java
+++ src/Basic/LeastCommanMultiple.java
@@ -10,19 +10,29 @@ public class LeastCommanMultiple {
 
         System.out.println("\n*** Welcome to the Lowest Comman Multiple Number Calculator ***\n");
 
-        System.out.print("Please enter a number: ");
+        System.out.print("Please enter the first number: ");
         int num1 = input.nextInt();
 
         System.out.print("Now, enter the second number: ");
         int num2 = input.nextInt();
 
-        int result = leastCommanMultiple();
+        int lcm = leastCommanMultiple(num1,num2);
+
+        System.out.println("LCM of your given number is: " + lcm );
 
     }
 
-    public static int leastCommanMultiple(){
+    public static int leastCommanMultiple(int num1, int num2){
 
-        return 0;
+        int i = 1;
+
+        while ( true ){
+            int factor = num1 * i;
+            if (factor % num2 == 0 ){
+                     return factor;
+                }
+            i++;
+        }
     }
 
 }
