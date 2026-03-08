c627629646a2cee3892ac8c291bf95cd80a700dd
p-suryavanshi <pavans6350@gmail.com>
2026-03-08 19:40:25 +0530

    Character to Interger Converter

---
+++ src/Basic/CharToIntergerConverter.java
@@ -0,0 +1,21 @@
+package Basic;
+import java.util.*;
+
+public class CharToIntergerConverter {
+
+    static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\n--- Welcome to the Character to Interger Converter ---\n");
+
+        System.out.print("Please enter a Character to Convert into Interger: ");
+        char ch = input.next().charAt(0);
+
+        int charToInt = ch;
+
+        System.out.println("Your Character " + "'" + ch + "'" + " is converted into Interger: " + charToInt);
+
+    }
+
+}
