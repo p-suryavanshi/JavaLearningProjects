fb9e1cb4d0d7eaba19a97f045b44d7239293403f
p-suryavanshi <pavans6350@gmail.com>
2026-03-08 22:07:44 +0530

    Sum of Five Numbers Calculator

---
+++ src/Basic/SumOfFiveNumbers.java
@@ -0,0 +1,29 @@
+package Basic;
+import java.util.*;
+
+public class SumOfFiveNumbers {
+
+    static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\n--- welcome to the Sum of Five Numbers Calculator ---\n");
+
+        System.out.print("Please enter the First Numbers To Calculate Sum: ");
+        int num1 = input.nextInt();
+        System.out.print("Please enter the Second Numbers To Calculate Sum: ");
+        int num2 = input.nextInt();
+        System.out.print("Please enter the Third Numbers To Calculate Sum: ");
+        int num3 = input.nextInt();
+        System.out.print("Please enter the Fourth Numbers To Calculate Sum: ");
+        int num4 = input.nextInt();
+        System.out.print("Please enter the Fifth Numbers To Calculate Sum: ");
+        int num5 = input.nextInt();
+
+        int sum = num1 + num2 + num3 + num4 + num5;
+
+        System.out.println("The Sum of your given five Numbers is: " + sum );
+
+    }
+
+}
