187d4960aaa13b421eb64565af155dd5f803e333
p-suryavanshi <pavans6350@gmail.com>
2026-03-12 21:21:23 +0530

    Circumference of circle calculator

---
+++ src/Basic/CircumferenceOfCircleCalculator.java
@@ -0,0 +1,20 @@
+package Basic;
+import java.util.*;
+
+public class CircumferenceOfCircleCalculator {
+
+    static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\n--- Welcome to the Circumference Of a Circle Calculator ---\n");
+
+        System.out.print("Please enter the radius of your circle: ");
+        int radius = input.nextInt();
+
+        double circumference = 2 * 3.14159 * radius;
+
+        System.out.println("The Circumference of your circle is: " + circumference);
+    }
+
+}
