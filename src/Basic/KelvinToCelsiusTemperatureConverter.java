e7bca679fc1962e75356de4c6de9568e741f94bc
p-suryavanshi <pavans6350@gmail.com>
2026-03-13 13:04:00 +0530

    Temperature converter from Kelvin to Celsius

---
+++ src/Basic/KelvinToCelsiusTemperatureConverter.java
@@ -0,0 +1,21 @@
+package Basic;
+import java.util.*;
+
+public class KelvinToCelsiusTemperatureConverter {
+
+    static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\n*** Welcome to the Temperature Converter from Kelvin to Celsiud ***\n");
+
+        System.out.print("Please enter the Temperature in Kelvin: ");
+        double kelvin = input.nextInt();
+
+        double celsius = kelvin - 273.15;
+
+        System.out.printf("Temperature in Celsius is: %.2f%n" , celsius , "C" );
+
+    }
+
+}
