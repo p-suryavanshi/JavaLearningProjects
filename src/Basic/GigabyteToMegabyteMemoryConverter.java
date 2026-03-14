cea0baf8bdcd40722255ee6b999445371799db38
p-suryavanshi <pavans6350@gmail.com>
2026-03-14 11:10:03 +0530

    Memory Converter from GB to MB

---
+++ src/Basic/GigabyteToMegabyteMemoryConverter.java
@@ -0,0 +1,21 @@
+package Basic;
+import java.util.*;
+
+public class GigabyteToMegabyteMemoryConverter {
+
+    static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\n*** Welcome to the Memory Converter from Gigabyte to Megabyte ***\n");
+
+        System.out.print("Please enter the Gigabyte(GB): ");
+        double gigabyte = input.nextDouble();
+
+        double megabyte = gigabyte * 1024;
+
+        System.out.println("The Megabyte of Gigabyte " + gigabyte + " is: " + megabyte + "MB");
+
+    }
+
+}
