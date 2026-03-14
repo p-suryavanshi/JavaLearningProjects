3dbee0b8d32ae480b59205135f4fd822d19e2fdf
p-suryavanshi <pavans6350@gmail.com>
2026-03-14 22:35:35 +0530

    Liter to Mililiter Converter

---
+++ src/Basic/LiterToMililitersConverter.java
@@ -0,0 +1,21 @@
+package Basic;
+import java.util.*;
+
+public class LiterToMililitersConverter {
+
+    static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("*** Welcome to the Liter to Militer Converter ***\n");
+
+        System.out.print("Please enter the Liter: ");
+        double liter = input.nextDouble();
+
+        double mililiter = liter * 1000;
+
+        System.out.println("Liter " + liter + "L is converted into Mililiter " + mililiter + "ML");
+
+    }
+
+}
