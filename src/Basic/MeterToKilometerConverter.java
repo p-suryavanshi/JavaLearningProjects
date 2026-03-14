dd3e489549209af3f19fa1583ac0c8692e58ac66
p-suryavanshi <pavans6350@gmail.com>
2026-03-14 22:28:53 +0530

    Meter to Kilometer Converter

---
+++ src/Basic/MeterToKilometerConverter.java
@@ -0,0 +1,21 @@
+package Basic;
+import java.util.*;
+
+public class MeterToKilometerConverter {
+
+    static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\n*** Welcome to the Meter to Kilometer Converter ***\n");
+
+        System.out.print("Please enter the Meter: ");
+        double meter = input.nextDouble();
+
+        double kiloMeter = meter / 1000;
+
+        System.out.println("Meter " + meter + " is converted into Kilometer: " + kiloMeter + "KM");
+
+    }
+
+}
