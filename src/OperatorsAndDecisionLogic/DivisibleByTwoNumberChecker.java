e791ef6ec65a41cf067ce893686283420f3c6fff
p-suryavanshi <pavans6350@gmail.com>
2026-03-15 20:30:54 +0530

    Number Checker If Number is Divisible by 2 or Not

---
+++ src/OperatorsAndDecisionLogic/DivisibleByTwoNumberChecker.java
@@ -0,0 +1,24 @@
+package OperatorsAndDecisionLogic;
+import java.util.Scanner;
+
+public class DivisibleByTwoNumberChecker {
+
+    static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\n*** Welcome to the Number Checker Number is Divisible by 2 or Not ***\n");
+
+        System.out.print("Please enter the Number: ");
+        int num = input.nextInt();
+
+        if ( num % 2 == 0){
+            System.out.println("Number " + num + " is Divisible by 2.");
+        }
+        else {
+            System.out.println("Number " + num + " is Not Divisible by 2.");
+        }
+
+    }
+
+}
