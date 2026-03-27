fd67c1cff65dcb730399cf280fab41390ef49547
p-suryavanshi <pavans6350@gmail.com>
2026-03-27 16:59:59 +0530

    The Gap Walker

---
+++ src/IterationControlLoopAndJumpStatements/ForLoop/TheGapWalkerToHundred.java
@@ -0,0 +1,15 @@
+package IterationControlLoopAndJumpStatements.ForLoop;
+
+public class TheGapWalkerToHundred {
+
+    public static void main(String[] a){
+
+        System.out.println("\n*** Welcome to the Gap Walker ***\n");
+
+        for ( int i = 1; i <= 100; i += 4 ) {
+            System.out.println(i);
+        }
+
+    }
+
+}
