6b303a17fa38b6b027e5381b0c018a30188d148f
p-suryavanshi <pavans6350@gmail.com>
2026-03-27 15:32:51 +0530

    Simple Counter Number that User want to 1

--- src/IterationControlLoopAndJumpStatements/ForLoop/SimpleCounter.java
+++ src/IterationControlLoopAndJumpStatements/ForLoop/SimpleCounter.java
@@ -1,4 +1,21 @@
 package IterationControlLoopAndJumpStatements.ForLoop;
+import java.util.*;
 
 public class SimpleCounter {
+
+    public static void main(String[] a) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\n*** Welcome to the Simple Counter ***\n");
+
+        System.out.print("Please enter a Number: ");
+        int num = sc.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            System.out.println(i);
+        }
+
+    }
+
 }
