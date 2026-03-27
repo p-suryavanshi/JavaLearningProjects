a3d47ee0f53867810a225e5192be4c2b57b94666
p-suryavanshi <pavans6350@gmail.com>
2026-03-27 14:50:00 +0530

    Factorial Finder of a Number Using For Loop

---
+++ src/IterationControlLoopAndJumpStatements/ForLoop/FactorialFinder.java
@@ -0,0 +1,25 @@
+package IterationControlLoopAndJumpStatements.ForLoop;
+import java.util.*;
+
+public class FactorialFinder {
+
+    public static void main(String[] a) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\n*** Welcome to the Factorial Finder ***\n");
+
+        System.out.print("Please enter the number: ");
+        int num = sc.nextInt();
+
+        int fact = 1;
+
+        for (int i = 1; i <= num; i++) {
+            fact = fact * i;
+        }
+
+        System.out.println("Factorial of " + num + " is: " + fact);
+
+    }
+
+}
