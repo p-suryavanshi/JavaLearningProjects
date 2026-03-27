fb5528088d9d1285450a2210f08cb43803ad0ad6
p-suryavanshi <pavans6350@gmail.com>
2026-03-27 15:45:19 +0530

    Odd Number Printer Using For Loop till user want

--- src/IterationControlLoopAndJumpStatements/ForLoop/OddStepper.java
+++ src/IterationControlLoopAndJumpStatements/ForLoop/OddStepper.java
@@ -1,4 +1,21 @@
 package IterationControlLoopAndJumpStatements.ForLoop;
+import java.util.*;
 
 public class OddStepper {
+
+    public static void main(String[] a) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\n*** Welcome to the Odd Stepper ***\n");
+
+        System.out.print("Please enter the Number till you want Odd Numbers: ");
+        int num = sc.nextInt();
+
+        for ( int i = 1; i <= num ; i += 2  ) {
+            System.out.println(i);
+        }
+
+    }
+
 }
