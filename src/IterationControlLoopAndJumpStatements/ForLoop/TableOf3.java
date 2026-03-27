014d2fbffaf1301e0e163bab84caf9ee2c2b3007
p-suryavanshi <pavans6350@gmail.com>
2026-03-27 16:49:45 +0530

    Table of 3 Printer Using For Loop

---
+++ src/IterationControlLoopAndJumpStatements/ForLoop/TableOf3.java
@@ -0,0 +1,18 @@
+package IterationControlLoopAndJumpStatements.ForLoop;
+import java.util.*;
+
+public class TableOf3 {
+
+    public static void main(String[] a) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\n*** Welcome to the Table of 3 Printer ***\n");
+
+        for ( int i = 3 ; i <= 30 ; i += 3 ) {
+            System.out.println(i);
+        }
+
+    }
+
+}
