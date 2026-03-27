bd7ab7b995bdb5369662dff08fccaf8b7ae9cc2f
p-suryavanshi <pavans6350@gmail.com>
2026-03-27 16:50:30 +0530

    Table Printer in even Using For Loop

--- src/IterationControlLoopAndJumpStatements/ForLoop/MultiStepEvenTable.java
+++ src/IterationControlLoopAndJumpStatements/ForLoop/MultiStepEvenTable.java
@@ -1,4 +1,20 @@
 package IterationControlLoopAndJumpStatements.ForLoop;
+import java.util.*;
 
 public class MultiStepEvenTable {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\n*** Welcome to the Multi Step Table ***\n");
+
+        System.out.print("Please enter a Number: ");
+        int num = sc.nextInt();
+
+        for ( int i = 2 ; i <= 10 ; i += 2 ) {
+            System.out.println(num + " X " + i + " = " + (num * i) );
+        }
+
+    }
 }
