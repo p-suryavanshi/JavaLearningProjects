81dd102925249ae7937c22439faba55dab6cc2cf
p-suryavanshi <pavans6350@gmail.com>
2026-04-01 14:34:09 +0530

    Strong Number Checker

--- src/IterationControlLoopAndJumpStatements/ForLoop/StrongNumberChecker.java
+++ src/IterationControlLoopAndJumpStatements/ForLoop/StrongNumberChecker.java
@@ -1,4 +1,34 @@
 package IterationControlLoopAndJumpStatements.ForLoop;
+import java.util.*;
 
 public class StrongNumberChecker {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\n*** Welcome to the Strong Number Checker ***\n");
+
+        System.out.print("Please enter the Number:  ");
+        int num = sc.nextInt();
+
+        int sum = 0;
+
+        for ( int i = num; i > 0; i /= 10 ) {
+            int digit = i % 10;
+            int fact = 1;
+            for ( int j = 1; j <= digit ; j++) {
+                fact *= j;
+            }
+            sum += fact;
+        }
+
+        if (sum == num) {
+            System.out.println(num + " is Strong Number");
+        } else {
+            System.out.println(num + " is Not Strong Number");
+        }
+
+    }
+
 }
