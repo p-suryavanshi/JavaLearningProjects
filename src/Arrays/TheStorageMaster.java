7c019c815cc3eb926cc95a323adb5caef8c8a59d
p-suryavanshi <pavans6350@gmail.com>
2026-04-05 00:24:50 +0530

    Storage Master

---
+++ src/Arrays/TheStorageMaster.java
@@ -0,0 +1,28 @@
+package Arrays;
+import java.util.Scanner;
+
+public class TheStorageMaster {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\n--- Welcome to the Storage Master ---\n");
+
+        System.out.print("How many items' prices do you want to store?: ");
+        int size = sc.nextInt();
+
+        double prices[] = new double[size];
+
+        for (int i = 0; i <= size-1; i++) {
+            System.out.print("Please enter the Price of item: ");
+            prices[i] = sc.nextDouble();
+        }
+
+        for (int j = 0; j <= size-1; j++) {
+            System.out.println(prices[j]);
+        }
+
+    }
+
+}
