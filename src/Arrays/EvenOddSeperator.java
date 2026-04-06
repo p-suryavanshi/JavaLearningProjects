e71f211885e1969228bfc46c33f682eff41b1ad5
p-suryavanshi <pavans6350@gmail.com>
2026-04-06 21:56:22 +0530

    Even Odd Seperator

---
+++ src/Arrays/EvenOddSeperator.java
@@ -0,0 +1,34 @@
+package Arrays;
+import java.util.Scanner;
+
+public class EvenOddSeperator {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\n*** Welcome to the Even Odd Seperator ***\n");
+
+        int []nums = new int[10];
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print("Please enter a number: ");
+            nums[i] = sc.nextInt();
+        }
+
+        for (int j = 0; j < nums.length-1; j++) {
+            if (nums[j] % 2 == 0) {
+                System.out.print("\nEven Number: " + nums[j]);
+            }
+        }
+
+        for (int k = 0; k < nums.length-1; k++) {
+            if (nums[k] % 2 != 0) {
+                System.out.print("\nOdd Numbers: " + nums[k]);
+            }
+        }
+
+    }
+
+}
+
