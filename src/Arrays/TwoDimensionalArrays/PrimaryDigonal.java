ee3dfaa4c8926b2cd9a80e56dfbe31cf674cdb11
p-suryavanshi <pavans6350@gmail.com>
2026-04-09 11:08:20 +0530

    Primary Diagonal X

---
+++ src/Arrays/TwoDimensionalArrays/PrimaryDigonal.java
@@ -0,0 +1,38 @@
+package Arrays.TwoDimensionalArrays;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PrimaryDigonal {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\n=== Welcome to the Primary Diagonal ===\n");
+
+        int [][]nums = new int[3][3];
+
+        for (int row = 0; row < nums.length; row++) {
+            for (int column = 0; column < nums[row].length; column++) {
+                System.out.print("Please enter Number: ");
+                nums[row][column] = sc.nextInt();
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
+                if (i == j) {
+                    System.out.print(nums[i][j] + " ");
+                } else if (i + j == 2) {
+                    System.out.print(nums[i][j] + " ");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
+}
