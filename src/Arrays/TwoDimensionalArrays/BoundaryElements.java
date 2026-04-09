7261804811e4255ad34b341059daa0861a67cb5f
p-suryavanshi <pavans6350@gmail.com>
2026-04-09 11:33:01 +0530

    Boundary Elements Printer

---
+++ src/Arrays/TwoDimensionalArrays/BoundaryElements.java
@@ -0,0 +1,35 @@
+package Arrays.TwoDimensionalArrays;
+import java.util.Scanner;
+
+public class BoundaryElements {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\n=== Welcome to the Boundary Elements Printer ===\n");
+
+        int [][]nums = new int[3][3];
+
+        for (int row = 0; row < nums.length; row++) {
+            for (int column = 0; column < nums[row].length; column++) {
+                System.out.print("Please enter Number: ");
+                nums[row][column] = sc.nextInt();
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
+                if (i != 1 || j != 1) {
+                    System.out.print(nums[i][j] + " ");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
+}
