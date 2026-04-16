366084f984efbfd1b3f8b122d97d63b8b3d6b590
p-suryavanshi <pavans6350@gmail.com>
2026-04-16 20:17:15 +0530

    Greet  massage function and calling

--- src/Functions/Greeter.java
+++ src/Functions/Greeter.java
@@ -1,4 +1,28 @@
 package Functions;
+import java.util.Scanner;
 
 public class Greeter {
+
+    public static String Greet(String name, String age ) {
+
+        return "\nYour Name is: " + name + "\nyour Age is: " + age;
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("\n=== Welcome to the Greeter ===");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("\nPlease enter your Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Now enter your Age: ");
+        String age = sc.nextLine();
+
+        System.out.println(Greet(name,age));
+
+    }
+
 }
