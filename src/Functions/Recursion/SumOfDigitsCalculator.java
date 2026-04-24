7679b209b574b6b43e1ab90de5de5c7975d53dd2
p-suryavanshi <pavans6350@gmail.com>
2026-04-24 16:43:29 +0530

    Sum of Digits Calculator

---
+++ src/Functions/Recursion/SumOfDigitsCalculator.java
@@ -0,0 +1,32 @@
+package Functions.Recursion;
+import java.util.Scanner;
+
+public class SumOfDigitsCalculator {
+
+    public static int DigitSum(int n) {
+
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        int div = n / 10;
+        int lastDigit = n % 10;
+
+        return lastDigit + DigitSum(div);
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\n=== Welcome to the Sum of Digits Calculator of a Number ===\n");
+
+        System.out.print("Please enter a Number: ");
+        int num = sc.nextInt();
+
+        System.out.println(DigitSum(num));
+
+    }
+
+}
